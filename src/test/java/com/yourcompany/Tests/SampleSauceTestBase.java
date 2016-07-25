e1bb7053b5ae1c9ebf9d8e536e1ce134723f0e41
gpradel13 <gpradel13@gmail.com>
2016-07-24 19:19:27 -0600

    Added a browser

--- src/test/java/com/yourcompany/Tests/SampleSauceTestBase.java
+++ src/test/java/com/yourcompany/Tests/SampleSauceTestBase.java
@@ -124,7 +124,7 @@ public class SampleSauceTestBase implements SauceOnDemandSessionIdProvider {
         LinkedList browsers = new LinkedList();
 
         // windows 7, Chrome 41
-        //browsers.add(new String[]{"Windows 7", "41", "chrome", null, null});
+        browsers.add(new String[]{"Windows 7", "41", "chrome", null, null});
 
         ///windows xp, IE 8
         browsers.add(new String[]{"Windows XP", "8", "internet explorer", null, null});
@@ -187,7 +187,9 @@ public class SampleSauceTestBase implements SauceOnDemandSessionIdProvider {
         }
         SauceHelpers.addSauceConnectTunnelId(capabilities);
         this.driver = new RemoteWebDriver(
+                //new URL("http://" + username+ ":" + accesskey + seleniumURI +"/wd/hub"),
                 new URL("https://" + username+ ":" + accesskey + seleniumURI +"/wd/hub"),
+
                 capabilities);
 
         this.sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
